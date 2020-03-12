e08f87c15a694b0ec5c250723b09197171869e1e
Udayan Kothari <kothari.udayan@hotmail.com>
2020-03-12 21:48:15 +0530

    Issue Fix with accumulators

--- src/main/scala/org/spark/handson/rdd/RddSharedVariables.scala
+++ src/main/scala/org/spark/handson/rdd/RddSharedVariables.scala
@@ -20,9 +20,14 @@ object RddSharedVariables extends App with SparkSessionConnection{
 
   val accRdd = spark.sparkContext.parallelize(Seq(1,2,3,4,5,6))
 
+  accRdd.map(x => accumulator.add(x))
+  // Here value of accumulator will be 0 as map is not an action
+  println("Value of accumulator is: " + accumulator.value)
+
   accRdd.foreach(x => accumulator.add(x))
 
   println("Value of accumulator is: " + accumulator.value)
 
+  Thread.sleep(300000)
 
 }
